f4668d2a4c5781f31a0735c5209a736f4bf658e0
Jared Cosulich <jared@irrationaldesign.com>
2013-08-01 22:15:59 -0700

    testing circuit with lightbulb

--- test/spec/circuitous/AnalyzerSpec.js
+++ test/spec/circuitous/AnalyzerSpec.js
@@ -13,7 +13,7 @@ describe("Analyzer", function() {
         })
 
         battery = createComponent(board, 'Battery')
-        onBoard = addToBoard(board, battery, 300, 300);
+        onBoard = addToBoard(board, battery, 100, 300);
         expect(onBoard).toBe(true);
     }); 
     
@@ -28,17 +28,44 @@ describe("Analyzer", function() {
             expect(circuit.complete).toBe(false);
         });        
         
-        describe('with some wire', function() {
-            beforeEach(function() {
+        describe('and wire', function() {            
+            it('should be an incomplete circuit with multiple components/wires when just a little is added', function() {
                 start = board.boardPosition(battery.currentNodes()[1]);
                 drawWire(board, start, 0, 3);
-            });
-            
-            it('should be an incomplete circuit with multiple components/wires', function() {
                 circuit = board.analyzer.run()
                 expect(Object.keys(circuit.components).length).toBeGreaterThan(1);
                 expect(circuit.complete).toBe(false); 
             });
+            
+            describe('completing the circuit', function() {
+                beforeEach(function() {
+                    start = board.boardPosition(battery.currentNodes()[1]);
+                    lastNode = drawWire(board, start, 0, 2);
+                    lastNode = drawWire(board, lastNode, 7, 0);
+                    lastNode = drawWire(board, lastNode, 0, -7);
+                    lastNode = drawWire(board, lastNode, -7, 0);
+                    lastNode = drawWire(board, lastNode, 0, 2);                    
+                });
+
+                it('should be a complete circuit with infinite amps', function() {
+                    circuit = board.analyzer.run()
+                    expect(circuit.complete).toBe(true);
+                    expect(circuit.amps).toBe('infinite') 
+                });
+
+                it('should not have resistance if a lightbulb is added', function() {
+                    bulb = createComponent(board, 'Lightbulb')
+                    node = wireAt(board, 16).nodes[0]
+                    onBoard = addToBoard(board, bulb, node.x, node.y);
+                    expect(onBoard).toBe(true);
+                    
+                    circuit = board.analyzer.run()
+                    expect(circuit.components[bulb.id]).toBe(true)
+                    expect(circuit.complete).toBe(true);
+                    expect(circuit.resistance).toBe(5) 
+                    expect(circuit.amps).toBe(1.8) 
+                });
+            })
         })
     });
     
@@ -62,8 +89,13 @@ createComponent = function(board, type) {
 
 drawWire = function(board, from, deltaX, deltaY) {
     offset = board.el.offset()
-    start = {clientX: offset.left + from.x, clientY: offset.top + from.y}
-    board.el.trigger('mousedown.draw_wire', start);    
-    board.wires.draw({clientX: start.clientX + (deltaX * board.cellDimension), clientY: start.clientY + (deltaY * board.cellDimension)});
+    board.el.trigger('mousedown.draw_wire', {clientX: offset.left + from.x, clientY: offset.top + from.y});
+    end  = {x: from.x + (deltaX * board.cellDimension), y: from.y + (deltaY * board.cellDimension)}
+    board.wires.draw({clientX: offset.left + end.x, clientY: offset.top + end.y});
     $(document.body).trigger('mouseup.draw_wire');
+    return end
+}
+
+wireAt = function(board, index) {
+    return board.wires.all()[Object.keys(board.wires.all())[index]]
 }
