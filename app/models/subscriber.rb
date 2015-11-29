908e3d25e5afc7cb54fcd9f0a5196aa9e01423b9
Jared Cosulich <jared.cosulich@gmail.com>
2015-11-29 17:22:35 -0500

    adding emails

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -1,2 +1,10 @@
 class Subscriber < ActiveRecord::Base
+  after_create :notify_admin
+
+  private
+  
+  def notify_admin
+    AdminMailer.notify("A new Puzzle School subscriber was created.", "New subscriber details:", {:subscriber => self}).deliver_later
+  end
+
 end
