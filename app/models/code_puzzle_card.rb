e9e22594e7cdcb28ea09282ec80268a96dda13b2
Jared Cosulich <jared.cosulich@gmail.com>
2017-10-29 15:28:42 -0400

    working on cards page

--- app/models/code_puzzle_card.rb
+++ app/models/code_puzzle_card.rb
@@ -2,4 +2,19 @@ class CodePuzzleCard < ApplicationRecord
 
   belongs_to :code_puzzle_group
 
+  CARDS = [
+    ["A1", "Move Forward"],
+    ["A2", "Move Backward"],
+    ["A3", "Rotate Right"],
+    ["A4", "Rotate Left"],
+    ["A5", "Fill Color"],
+    ["P1", "Pen Up"],
+    ["P2", "Pen Down"],
+    ["P3", "Pen Size"],
+    ["P4", "Pen Color"],
+    ["F1", "Function"],
+    ["F2", "End Function"],
+    ["L1", "Loop"],
+    ["L2", "End Loop"]
+  ]
 end
