59fec76ee4a66d774639902f1c02c06a09ce81e0
Jared Cosulich <jared.cosulich@gmail.com>
2017-10-30 21:31:49 -0400

    working on code puzzle flow

--- app/controllers/code_puzzle_classes_controller.rb
+++ app/controllers/code_puzzle_classes_controller.rb
@@ -2,7 +2,7 @@ class CodePuzzleClassesController < ApplicationController
   before_action :set_code_puzzle_class, only: [:show, :edit, :update, :destroy]
 
   def cards
-    @code_puzzle_form = CodePuzzleForm.new(payment: 300)
+    @code_puzzle_form = CodePuzzleForm.new(payment: 500)
   end
 
   # GET /code_puzzle_classes
