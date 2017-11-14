01d92b01f2d356f8dcdbd6bef0409fd4fdc6f9b9
Jared Cosulich <jared.cosulich@gmail.com>
2017-11-13 22:30:22 -0500

    trying to get js working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-// require jquery
+//= require jquery
 // require bootstrap
 // require jquery_ujs
 // require turbolinks
-//= require_tree .
+// require_tree .
 
 alert(0);
 alert(1);
