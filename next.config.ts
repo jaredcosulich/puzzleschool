a610116763fcc51bc69f338da125f05cb1c8d606
Jared Cosulich <jared@codeyam.com>
2024-11-19 15:21:49 -0500

    fixing next.config.ts to work with localhost

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProduction = process.env.NODE_ENV === 'production'
+const repoName = 'puzzleschool'
+
 const nextConfig: NextConfig = {
-  basePath: "/puzzleschool",
+  basePath: isProduction ? `/${repoName}` : '',
   output: "export",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
