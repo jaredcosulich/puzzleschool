da2a6c4f435c15669c3735e916aa21471087558c
Jared Cosulich <jared@codeyam.com>
2024-11-19 15:18:50 -0500

    revert image paths

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
 
 export default function Home() {
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
-          src={`/puzzleschool/next.svg`}
+          src={`/next.svg`}
           alt="Next.js logo"
           width={180}
           height={38}
@@ -32,7 +33,7 @@ export default function Home() {
           >
             <Image
               className="dark:invert"
-              src="/puzzleschool/vercel.svg"
+              src="/vercel.svg"
               alt="Vercel logomark"
               width={20}
               height={20}
