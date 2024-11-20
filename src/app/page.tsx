da007c490181bb8e8fb3b194ee555431888a94bb
Jared Cosulich <jared@codeyam.com>
2024-11-20 12:06:51 -0500

    adding logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
+import Image from "next/image";
+
 export default function Home() {
 
   return (
-    <div className='flex flex-col gap-6 p-64 text-center'>
-      <div className='text-2xl font-semibold'>
-        The Puzzle School
+    <div className='flex flex-col items-center gap-6 p-64 text-center'>
+      <Image 
+        src='/images/compass_logo.png' 
+        alt="The Puzzle School"
+        width={349}
+        height={132}
+        className='mr-[72px]'
+      />
+      <div>
+        An educational philosophy focused on developing the metacognitive skills for navigating ambiguity.
       </div>
       <div>More coming soon...</div>
     </div>
